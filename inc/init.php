4071f9611855a59f0cdd932abf2e1a0db5533f18
Sascha Wäschle <sascha.waeschle@d4ho.de>
2014-02-18 23:49:31 +0100

    Add Comments to the init.php file

--- inc/init.php
+++ inc/init.php
@@ -1,30 +1,41 @@
 <?
-$debug = "";
 function init($content)
 {
-	//Loading plugin modules
+	//Global Path Variable
 	global $path;
+	
+	//Loading Plugin Folder
 	$plugins=opendir($path['plugins']);
+	
+	//Get Main the File for this Page (index.html)
 	$output = getFile($content);
+	
+	//Loading the Plugins
 	while ($plugin = readdir($plugins)) 
 	{
-		//Loading File funktion
+		//Loading Plugin functions
 		if ($plugin != ".." && $plugin != ".") 
 		{
 			debug("loading $plugin");
+			//Import plugin function
 			includeFile($path['plugins'].$plugin);
+			//Remove .php
 			$plugin = substr($plugin,0,-4);
+			//Replace the Tag with the returning content from the plugin
 			$output = show($output, array( $plugin => $plugin()));
 			debug("[".ucwords($plugin)."] - checked and loaded");
 		}
 	}
 	closedir($plugins);
+	
+	//Output Debug, Errors and Content
 	echo debugOutput();
 	echo $output;
 }
 
 function debugOutput()
 {
+	//Display alle Errors and the Log
 	global $debug;
 	$output = "Log:<br>";
 	$output .= $debug;
@@ -37,6 +48,7 @@ function debugOutput()
 
 function debug($add)
 {
+	//function to add Debug inputs
 	global $debug;
 	$debug .= "[".gmdate("H:i:s", time())."] - ".$add."<br/>";
 }
