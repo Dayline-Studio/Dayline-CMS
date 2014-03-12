0fc70416c9b0862b0a9f98e29cb94347dcd830fa
Trax2oooK1ng <2000traxer@gmail.com>
2014-03-12 20:33:51 +0100

    Update contact site
    essential content

--- pages/contact.php
+++ pages/contact.php
@@ -3,6 +3,44 @@
 /**--**/ include "../inc/config.php";
 //------------------------------------------------
 // Site Informations
-/**--**/  $meta['title'] = "Contact";
+/**--**/  $meta['title'] = "Kontakt";
 //------------------------------------------------
+if ($do=='')
+{
+    switch ($show)
+    {
+        default:
+        $out = show('contact/form');
+        break;
+    }
+} else {
+    switch ($do)
+    {
+        case send_mail:
+            if($_POST['mail'] == "" ||
+               $_POST['subject'] == "" ||
+               $_POST['message'] == "")
+            {
+                $out = msg(_fields_missing); 
+            }
+            else if (!check_email_address($_POST['mail']))
+            {
+                $out = msg(_mailcheck_failed);
+            }
+            else {
 
+            up("INSERT INTO messages (id,mail,subject,message,targetgroup,to_user)"
+                    . "VALUES (NULL, ".sqlString($_POST['mail']).", ".sqlString($_POST['subject']).", ".sqlString($_POST['message']).", 1, 0");
+
+                if(mail(con($_POST['mail']),con($_POST['subject']) , con($_POST['message']))) {
+                    $out = msg(_mail_send_true);
+                } else {
+                    $out = msg(_mail_send_false);
+                }
+             }
+             break;
+    }
+}
+
+//Initialisierung
+init($out, $meta);
