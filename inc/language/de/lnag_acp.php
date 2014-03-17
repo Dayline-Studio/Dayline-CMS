3fc8562e83ec2d009c1372ffeae19a7a5b14b216
rayw3n <rayw3n@gmail.com>
2014-03-17 20:33:51 +0100

    Add ACP Language

---
+++ inc/language/de/lnag_acp.php
@@ -0,0 +1,10 @@
+<?php
+define ('_title_site', "Titelseite");
+define ('_keywords', "Suchbegriffe");
+define ('_description', "Beschreibung");
+define ('_addafter', "Unterordnen");
+define ('_group', "Gruppe");
+define ('_news_content', "News");
+define ('_enter_content_here', "News  hier eingeben...");
+define ('_show_public', "Sichtbar");
+define ('_main_image', "Bildlink");
