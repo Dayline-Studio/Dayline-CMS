7c4baa7e412b809014ef940db513bf260670c160
Trax2oooK1ng <2000traxer@gmail.com>
2014-03-24 22:53:21 +0100

    Update lang_counter.php

--- content/language/de/lang_counter.php
+++ content/language/de/lang_counter.php
@@ -1,3 +1,6 @@
-<?
-//ToDo counter language
-?>
+<?php
+define ('_count_today', 'Heute Aufrufe');
+define ('_count_total', 'Insg. Aufrufe');
+define ('_users_today', 'Heute Besucher');
+define ('_users_total', 'Insg. Besucher');
+define ('_daily_avg', 'Durchs. Aufrufe/Tag');
