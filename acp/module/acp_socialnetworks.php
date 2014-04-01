f8169f14b042ec45efed0a75ec1c893ea1795d06
Sascha Wäschle <sascha.waeschle@d4ho.de>
2014-04-01 23:10:47 +0200

    Update SocialNetwork ACP

--- acp/module/acp_socialnetworks.php
+++ acp/module/acp_socialnetworks.php
@@ -1,8 +1,31 @@
 <?php
- switch($action)
- {
-     default:
-         $disp = show('acp/acp_socialnetworks');
-         break;
- }
+if ($do == '') { 
+    switch($action)
+     {
+         default:
+             $disp = show('acp/acp_socialnetworks');
+             break;
+     }
+} else {
+    switch ($do)
+    {
+        case 'update':
+            if (permTo('update_socialnetwork')) {
+                if (up('Update settings SET ('
+                        . 'link_twitter = '.sqlStringCon($_POST['twitter']).','
+                        . 'link_facebook = '.sqlStringCon($_POST['facebook']).','
+                        . 'link_google = '.sqlStringCon($_POST['google']).','
+                        . 'link_youtube = '.sqlStringCon($_POST['youtube']).''
+                        . ' LIMIT 1')) {
+                    $disp = msg(_change_sucessful);
+                } else {
+                    $disp = msg(_change_failed);
+                }    
+            } else { 
+                $disp = msg(_no_permissions);
+            }
+            break;
+    }
+}
+ 
 
