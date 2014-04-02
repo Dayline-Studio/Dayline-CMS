3a6a44590ef02a7df067412bc540350168be5a30
Trax2oooK1ng <2000traxer@gmail.com>
2014-04-02 08:34:13 +0200

    Update lang_msg.php

--- content/language/en/lang_msg.php
+++ content/language/en/lang_msg.php
@@ -1,13 +1,31 @@
-<?<?php
-define ('_pass_dont_match', 		"Passwords do not match");
-define ('_pass_wrong', 				"Current password is wrong");
-define ('_fields_missing', 			"There are not filled in all teh required fields");
-define ('_change_sucessful', 		"Change successfully!");
-define ('_login_failed', 			"Password or Username is wrong!");
-define ('_login_successful', 		"Login successfully!");
-define ('_welcome_evening', 		"Good evening [user].");
-define ('_welcome_night', 			"Good night [user].");
-define ('_welcome_morning', 		"Good morning [user].");
-define ('_welcome_noon', 		    "Good noon[user].");
-define ('_welcome_afternoon', 		"Good afternoon [user].");
- 
+<?php
+define ('_pass_dont_match', 		"passwords don't match");
+define ('_pass_wrong', 				"wrong password");
+define ('_fields_missing', 			"there are some missing boxes");
+define ('_change_sucessful', 		"change successful");
+define ('_change_failed', 			"change failed");
+define ('_login_failed', 			"login failed username and password don't match");
+define ('_login_successful', 		"login successful");
+define ('_welcome_evening', 		"good evening [user].");
+define ('_welcome_night', 			"good night [user].");
+define ('_welcome_morning', 		"good morning [user].");
+define ('_welcome_noon', 		    "good day [user].");
+define ('_welcome_afternoon', 		"good afternoon [user].");
+define ('_no_permissions',		 	"you have no permission to view this site");
+define ('_site_created_sucessful',	"site successful created");
+define ('_mailcheck_failed',		"e-mail address not valid");
+define ('_already_exists', 			"username already taken");
+define ('_regist_sucess', 			"register successful you can now log into your account have fun");
+define ('_regist_failed', 			"an error happened please try again or contact an admin");
+define ('_wrong_pw', 				"wrong password"); // use _pass_wrong???
+define ('_user_not_found', 			"there is no account associated with that username");
+define ('_comment_successful', 		"done");
+define ('_comment_no_permissions', 	"you don't have the permissions to comment");
+define ('_comment_site_not_found', 	"site not found");
+define ('_comment_failed', 			"comment failed");
+define ('_comment_content_not_found', 	"please fill in the comment form");
+define ('_not_logged_in', "you have to be logged in to use this function"); 
+define ('_mail_send_true', "e-mail sended");
+define ('_mail_send_false', "problem with sending please check your input");  
+define ('_no_server_found', "you don't have a server yet");
+define ('_categorie_not_found', "category not found");
