ab5bc49ef84123674a2a06cad2d02044c7ad2d80
Trax2oooK1ng <2000traxer@gmail.com>
2014-04-02 08:28:11 +0200

    Update lang_counter.php

--- content/language/de/lang_counter.php
+++ content/language/de/lang_counter.php
@@ -1,6 +1,6 @@
 <?php
-define ('_count_today', 'Heute Aufrufe');
+define ('_count_today', 'Heutige Aufrufe');
 define ('_count_total', 'Insg. Aufrufe');
-define ('_users_today', 'Heute Besucher');
+define ('_users_today', 'Heutige Besucher');
 define ('_users_total', 'Insg. Besucher');
 define ('_daily_avg', 'Durchs. Aufrufe/Tag');
